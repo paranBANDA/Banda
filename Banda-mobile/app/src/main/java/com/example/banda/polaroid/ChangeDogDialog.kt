00abbc043b7d4ba0b21bf3f367359ada7ec083e0
exd0tpy <jsw5258@ajou.ac.kr>
2022-11-11 14:06:22 +0900

    load dog information at ChangeDogDialog

--- Banda-mobile/app/src/main/java/com/example/banda/polaroid/ChangeDogDialog.kt
+++ Banda-mobile/app/src/main/java/com/example/banda/polaroid/ChangeDogDialog.kt
@@ -9,25 +9,83 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.kakaologin.DogProfileAdapter
 import com.example.banda.DogProfileData
 import com.example.banda.PolariodAdapter
+import com.example.banda.RetrofitService
+import com.example.banda.data.Groupcheck
+import com.example.banda.data.UserPet
 import com.example.banda.databinding.ChangeDogDialogBinding
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.text.SimpleDateFormat
 
 class ChangeDogDialog(context: Context): Dialog(context) {
 
     private lateinit var binding: ChangeDogDialogBinding
 
+    val datas = mutableListOf<DogProfileData>()
+    var email = ""
+    private var service: RetrofitService? = null
+    private fun loadUserEmail(){
+        val pref = context.getSharedPreferences("pref",0)
+        email = pref?.getString("email","").toString()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // 만들어놓은 dialog_profile.xml 뷰를 띄운다.
+        loadUserEmail()
         binding = ChangeDogDialogBinding.inflate(layoutInflater)
         // 뒤로가기 버튼, 빈 화면 터치를 통해 dialog가 사라지지 않도록
         //setCancelable(false)
-        val datas = mutableListOf<DogProfileData>()
-        datas.apply{
+        binding.changeDogDialogRecyclerView.adapter = DogProfileAdapter(datas)
+        val retrofit = Retrofit.Builder().baseUrl("http://13.124.202.212:3000/")
+            .addConverterFactory(GsonConverterFactory.create()).build();
+        service = retrofit.create(RetrofitService::class.java)
+        val UserRequest = Groupcheck(email = email)
+        runBlocking {
+            launch {
+                service?.ShowUserPet(UserRequest)?.enqueue(object : Callback<UserPet> {
+                    override fun onResponse(call: Call<UserPet>, response: Response<UserPet>) {
+                        if (response.isSuccessful) {
+                            val body = response.body()
+                            if (body != null) {
+                                var petData = body.data
+                                for (i in petData) {
+                                    datas.apply {
+                                        add(
+                                            DogProfileData(
+                                                name = i.petname,
+                                                birth = SimpleDateFormat("yyyy-MM-dd").format(i.birthday),
+                                                img = "",
+                                                gender = i.gender,
+                                                breed = i.breed,
+                                                meetday = SimpleDateFormat("yyyy-MM-dd").format(i.meetday),
+                                                petId = 1
+                                            )
+                                        )
+                                        println(datas)
+                                    }
+                                }
+                                binding.changeDogDialogRecyclerView.adapter?.notifyDataSetChanged()
+                            }
+                        } else {
+                            println("실패")
+                        }
+                    }
 
+                    override fun onFailure(call: Call<UserPet>, t: Throwable) {
+                        // 통신 실패 (인터넷 끊킴, 예외 발생 등 시스템적인 이유)
+                        println("에러: " + t.message.toString());
+                    }
+                })
+            }
         }
 
 
-        binding.changeDogDialogRecyclerView.adapter = DogProfileAdapter(datas)
         binding.changeDogDialogRecyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         setContentView(binding.root)
         initViews()
