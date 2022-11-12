519a0a0e002430003fb01629b1320ca183e5d2d0
exd0tpy <jsw5258@ajou.ac.kr>
2022-11-12 23:10:31 +0900

    Set listener on walk start FAB

--- Banda-mobile/app/src/main/java/com/example/banda/MainActivity.kt
+++ Banda-mobile/app/src/main/java/com/example/banda/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.banda
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +9,7 @@ import androidx.fragment.app.Fragment
 import com.example.banda.MyPage.MyPageFragment
 import com.example.banda.store.StoreFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity(){
     var polaroidFragment: Fragment? = null
@@ -15,6 +18,15 @@ class MainActivity : AppCompatActivity(){
     var bottomAppBar: BottomNavigationView? = null
     var myPageFragment : MyPageFragment? = null
 
+    var walkButton : FloatingActionButton? = null
+
+    private fun fabColorActivate() {
+        walkButton?.imageTintList = ColorStateList.valueOf(Color.rgb(0xff, 0xc1, 0x1a))
+    }
+
+    private fun fabColorDeactivate() {
+        walkButton?.imageTintList = ColorStateList.valueOf(Color.rgb(0x87, 0x87, 0x87))
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,13 +35,19 @@ class MainActivity : AppCompatActivity(){
         mainPageFragment = MainPageFragment()
         myPageFragment = MyPageFragment()
         bottomAppBar = findViewById<BottomNavigationView>(R.id.bottomAppBar)
-
+        walkButton = findViewById<FloatingActionButton>(R.id.walkButton)
+        walkButton?.setOnClickListener {
+            fabColorActivate()
+            supportFragmentManager.beginTransaction().replace(R.id.frame,
+                polaroidFragment!!
+            ).commit()
+        }
         bottomAppBar?.setOnItemSelectedListener { item ->
 
             when(item.itemId) {
 
                 R.id.navHome -> {
-
+                    fabColorDeactivate()
                     Log.d("ASD", item.itemId.toString())
                     supportFragmentManager.beginTransaction().replace(R.id.frame ,
                         mainPageFragment!!
@@ -37,6 +55,7 @@ class MainActivity : AppCompatActivity(){
                     true
                 }
                 R.id.navPolaroid -> {
+                    fabColorDeactivate()
 
                     Log.d("ASD", item.itemId.toString())
                     supportFragmentManager.beginTransaction().replace(R.id.frame,
@@ -45,6 +64,7 @@ class MainActivity : AppCompatActivity(){
                     true
                 }
                 R.id.navWalk -> {
+                    fabColorActivate()
                     Log.d("ASD", item.itemId.toString())
                     supportFragmentManager.beginTransaction().replace(R.id.frame,
                         polaroidFragment!!
@@ -52,6 +72,7 @@ class MainActivity : AppCompatActivity(){
                     true
                 }
                 R.id.navStore -> {
+                    fabColorDeactivate()
                     Log.d("ASD", item.itemId.toString())
 //                    supportFragmentManager.beginTransaction().replace(R.id.frame,
 //                        storeFragment!!
@@ -59,6 +80,7 @@ class MainActivity : AppCompatActivity(){
                     true
                 }
                 R.id.navMypage -> {
+                    fabColorDeactivate()
                     Log.d("ASD", item.itemId.toString())
                     supportFragmentManager.beginTransaction().replace(R.id.frame,
                         myPageFragment!!
